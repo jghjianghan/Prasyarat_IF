3545041367650938e8148d90435ed0c4f1f36817
Jiang Han <jianghan99@gmail.com>
2021-12-08 17:34:55 +0700

    Revisi model MataKuliah

--- app/src/main/java/id/ac/unpar/informatika/prasyaratif/model/MataKuliah.java
+++ app/src/main/java/id/ac/unpar/informatika/prasyaratif/model/MataKuliah.java
@@ -2,10 +2,10 @@ package id.ac.unpar.informatika.prasyaratif.model;
 
 import java.util.List;
 
+/**
+ * Model untuk Mata Kuliah
+ */
 public class MataKuliah {
-    /**
-    * Model untuk Mata Kuliah
-    */
 
     private String nama;
     private String kode;
@@ -15,7 +15,7 @@ public class MataKuliah {
     private List<MataKuliah> prasyaratTempuh;
     private List<MataKuliah> prasyaratLulus;
     private List<MataKuliah> prasyaratBersamaan;
-    private int berlakuAngkatan;
+    private Integer berlakuAngkatan;
     private boolean isFavorite;
     private int prasyaratWajibBagi;
     private int prasyaratLulusBagi;
@@ -26,8 +26,7 @@ public class MataKuliah {
                       int semester,
                       boolean wajib,
                       int sks
-                      )
-    {
+    ) {
         this.nama = nama;
         this.kode = kode;
         this.semester = semester;
@@ -40,12 +39,11 @@ public class MataKuliah {
             List<MataKuliah> prasyaratTempuh,
             List<MataKuliah> prasyaratLulus,
             List<MataKuliah> prasyaratBersamaan,
-            int berlakuAngkatan,
+            Integer berlakuAngkatan,
             boolean isFavorite,
             int prasyaratWajibBagi,
             int prasyaratLulusBagi
-    )
-    {
+    ) {
         this.prasyaratTempuh = prasyaratTempuh;
         this.prasyaratLulus = prasyaratLulus;
         this.prasyaratBersamaan = prasyaratBersamaan;
@@ -53,6 +51,43 @@ public class MataKuliah {
         this.isFavorite = isFavorite;
         this.prasyaratWajibBagi = prasyaratWajibBagi;
         this.prasyaratLulusBagi = prasyaratLulusBagi;
+    }
+
+    /**
+     * Ini konstruktor yang lengkap
+     * @param nama Nama mata kuliah
+     * @param kode Kode mata kuliah
+     * @param semester Semester mata kuliah
+     * @param wajib Apakah mata kuliah ini wajib?
+     * @param sks Jumlah sks dari mata kuliah ini
+     * @param prasyaratTempuh Daftar prasyarat tempuh dari mata kuliah ini
+     * @param prasyaratLulus Daftar prasyarat lulus dari mata kuliah ini
+     * @param prasyaratBersamaan Daftar prasyarat bersamaan dari mata kuliah ini
+     * @param berlakuAngkatan Berlaku mulai angkatan?
+     * @param isFavorite Apakah mata kuliah ini merupakan mata kuliah favorit?
+     */
+    public MataKuliah(
+            String nama,
+            String kode,
+            int semester,
+            boolean wajib,
+            int sks,
+            List<MataKuliah> prasyaratTempuh,
+            List<MataKuliah> prasyaratLulus,
+            List<MataKuliah> prasyaratBersamaan,
+            Integer berlakuAngkatan,
+            boolean isFavorite
+    ) {
+        this.nama = nama;
+        this.kode = kode;
+        this.semester = semester;
+        this.wajib = wajib;
+        this.sks = sks;
+        this.prasyaratTempuh = prasyaratTempuh;
+        this.prasyaratLulus = prasyaratLulus;
+        this.prasyaratBersamaan = prasyaratBersamaan;
+        this.berlakuAngkatan = berlakuAngkatan;
+        this.isFavorite = isFavorite;
     }
 
     public String getNama() {
@@ -75,31 +110,48 @@ public class MataKuliah {
         return sks;
     }
 
-    public List<MataKuliah> getPrasyaratTempuh(){return prasyaratTempuh;}
+    public List<MataKuliah> getPrasyaratTempuh() {
+        return prasyaratTempuh;
+    }
 
-    public List<MataKuliah> getPrasyaratLulus(){return prasyaratLulus;}
+    public List<MataKuliah> getPrasyaratLulus() {
+        return prasyaratLulus;
+    }
 
-    public List<MataKuliah> getPrasyaratBersamaan(){return prasyaratBersamaan;}
+    public List<MataKuliah> getPrasyaratBersamaan() {
+        return prasyaratBersamaan;
+    }
 
-    public int getBerlakuAngkatan(){return berlakuAngkatan;}
+    public Integer getBerlakuAngkatan() {
+        return berlakuAngkatan;
+    }
 
-    public boolean getIsFavorite(){return isFavorite;}
+    public boolean getIsFavorite() {
+        return isFavorite;
+    }
 
-    public void setIsFavorite(boolean isFavorite){
+    public void setIsFavorite(boolean isFavorite) {
         this.isFavorite = isFavorite;
     }
 
-    public boolean toggleIsFavorite(){return isFavorite;}
+    public boolean toggleIsFavorite() {
+        isFavorite = !isFavorite;
+        return isFavorite;
+    }
 
-    public int getPrasyaratLulusBagi(){return prasyaratLulusBagi;}
+    public int getPrasyaratLulusBagi() {
+        return prasyaratLulusBagi;
+    }
 
-    public void setPrasyaratLulusBagi(int prasyaratLulusBagi){
+    public void setPrasyaratLulusBagi(int prasyaratLulusBagi) {
         this.prasyaratLulusBagi = prasyaratLulusBagi;
     }
 
-    public int getPrasyaratWajibBagi(){return prasyaratWajibBagi;}
+    public int getPrasyaratWajibBagi() {
+        return prasyaratWajibBagi;
+    }
 
-    public void setPrasyaratWajibBagi(int prasyaratWajibBagi){
+    public void setPrasyaratWajibBagi(int prasyaratWajibBagi) {
         this.prasyaratWajibBagi = prasyaratWajibBagi;
     }
 
